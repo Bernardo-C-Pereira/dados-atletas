d056eb7d4886546bc998bc0b8362c96c6feb1395
Bernardo-C-Pereira <bernardocpereira2007@gmail.com>
2025-09-04 16:41:57 -0300

    Código 1.1

--- dados-atletas.js
+++ dados-atletas.js
@@ -7,14 +7,37 @@ class Atleta {
         this.notas = notas;
     }
     calculaCategoria() {
+        if (this.idade >= 9 && this.idade <= 11) {
+            return "Infantil";
+        }
+
+        if (this.idade >= 12 && this.idade <= 13) {
+            return "Juvenil";
+        }
+
+        if (this.idade >= 14 && this.idade <= 15) {
+            return "Intermediário";
+        }
+
+        if (this.idade >= 16 && this.idade <= 30) {
+            return "Adulto";
+        }
+
+        return "Sem categoria";
 
     }
 
     calculaIMC() {
+        return (this.peso / (this.altura * this.altura)).toFixed(2);
 
     }
 
     calculaMediaValida() {
+        let notasOrdenadas = this.notas.slice().sort((a, b) => a - b);
+        notasOrdenadas.pop();
+        notasOrdenadas.shift();
+        let soma = notasOrdenadas.reduce((acc, nota) => acc + nota, 0);
+        return (soma / notasOrdenadas.length).toFixed(2);
 
     }
 
@@ -51,3 +74,19 @@ class Atleta {
     }
 
 }
+
+const atleta = new Atleta("Cesar Abascal",
+    30, 80, 1.70,
+    [10, 9.34, 8.42, 10, 7.88]);
+
+console.log(atleta.calculaCategoria());    
+console.log(atleta.calculaIMC());
+console.log(atleta.calculaMediaValida());
+console.log(atleta.obtemNomeAtleta());
+console.log(atleta.obtemIdadeAtleta());
+console.log(atleta.obtemPesoAtleta());
+console.log(atleta.obtemAlturaAtleta());
+console.log(atleta.obtemNotasAtleta());
+console.log(atleta.obtemCategoria());
+console.log(atleta.obtemIMC());
+console.log(atleta.obtemMediaValida());
