60a17886e06eb7b115215c2b80fe9349d7693e86
Bernardo-C-Pereira <bernardocpereira2007@gmail.com>
2025-09-04 17:28:22 -0300

    Código 1.3

--- dados-atletas.js
+++ dados-atletas.js
@@ -24,12 +24,10 @@ class Atleta {
         }
 
         return "Sem categoria";
-
     }
 
     calculaIMC() {
         return (this.peso / (this.altura * this.altura)).toFixed(2);
-
     }
 
     calculaMediaValida() {
@@ -38,62 +36,56 @@ class Atleta {
         notasOrdenadas.shift();
         let soma = notasOrdenadas.reduce((acc, nota) => acc + nota, 0);
         return (soma / notasOrdenadas.length).toFixed(2);
-
     }
 
     obtemNomeAtleta() {
         return this.nome;
-
     }
 
     obtemIdadeAtleta() {
         return this.idade;
-
     }
 
     obtemPesoAtleta() {
         return this.peso;
-
     }
 
     obtemAlturaAtleta() {
         return this.altura;
-
     }
 
     obtemNotasAtleta() {
         return this.notas.join(", ");
-
     }
 
     obtemCategoria() {
         return this.calculaCategoria();
-
     }
 
     obtemIMC() {
         return this.calculaIMC();
-
     }
 
     obtemMediaValida() {
         return this.calculaMediaValida();
     }
-
 }
 
-const atleta = new Atleta("Cesar Abascal",
-    30, 80, 1.70,
-    [10, 9.34, 8.42, 10, 7.88]);
+function obterDadosAtleta(atleta) {
+    return `Nome: ${atleta.obtemNomeAtleta()}
+Idade: ${atleta.obtemIdadeAtleta()}
+Peso: ${atleta.obtemPesoAtleta()} Kg
+Altura: ${atleta.obtemAlturaAtleta()} m
+Notas: ${atleta.obtemNotasAtleta()}
+Categoria: ${atleta.obtemCategoria()}
+IMC: ${atleta.obtemIMC()}
+Média Válida: ${atleta.obtemMediaValida()}`;
+}
 
-console.log(atleta.calculaCategoria());    
-console.log(atleta.calculaIMC());
-console.log(atleta.calculaMediaValida());
-console.log(atleta.obtemNomeAtleta());
-console.log(atleta.obtemIdadeAtleta());
-console.log(atleta.obtemPesoAtleta());
-console.log(atleta.obtemAlturaAtleta());
-console.log(atleta.obtemNotasAtleta());
-console.log(atleta.obtemCategoria());
-console.log(atleta.obtemIMC());
-console.log(atleta.obtemMediaValida());
+const atleta1 = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88]);
+
+const atleta2 = new Atleta("Pedro Pascal", 40, 85, 1.74, [10, 9.30, 8.40, 10, 7.80]);
+
+console.log(obterDadosAtleta(atleta1));
+console.log("----");
+console.log(obterDadosAtleta(atleta2));
