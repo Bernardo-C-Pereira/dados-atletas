7dd4292c8202483fb223bbb7829dc3a6345f14ea
Bernardo-C-Pereira <bernardocpereira2007@gmail.com>
2025-09-04 16:17:02 -0300

    Código 1.0

---
+++ dados-atletas.js
@@ -0,0 +1,53 @@
+class Atleta {
+    constructor(nome, idade, peso, altura, notas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+        this.notas = notas;
+    }
+    calculaCategoria() {
+
+    }
+
+    calculaIMC() {
+
+    }
+
+    calculaMediaValida() {
+
+    }
+
+    obtemNomeAtleta() {
+
+    }
+
+    obtemIdadeAtleta() {
+
+    }
+
+    obtemPesoAtleta() {
+
+    }
+
+    obtemAlturaAtleta() {
+
+    }
+
+    obtemNotasAtleta() {
+
+    }
+
+    obtemCategoria() {
+
+    }
+
+    obtemIMC() {
+
+    }
+
+    obtemMediaValida() {
+
+    }
+
+}
